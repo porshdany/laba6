c6e89ef177239da60e0f1b6f23836de71e11b616
porshdany <porsh-porsh@inbox.ru>
2020-03-26 16:05:56 +0300

    first

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /cygdrive/d/Projects/laba6
+# Build directory: /cygdrive/d/Projects/laba6/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit_tests "tests")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/cygdrive/d/Projects/laba6/CMakeLists.txt;62;add_test;/cygdrive/d/Projects/laba6/CMakeLists.txt;0;")
